c72943487115d5e93a33be51af5aecf4efc74f51
Doing <1211126374@qq.com>
2018-09-04 09:32:49 +0800

    src

--- acfun/test/spider_demo.js
+++ acfun/test/spider_demo.js
@@ -3,14 +3,29 @@ const cherrio = require('cheerio');
 
 (async () => {
 
-  const res = await axios.get('http://www.acfun.cn/a/ac4127544');
+  const res = await axios.get('http://www.acfun.cn/a/ac4126053');
   const html = res.data;
 
   const $ = cherrio.load(html);
   const articleContent = $('.article-content');
 
-  const p_text = articleContent.children('p').text();
-  console.log(p_text)
+  const doms = $(articleContent).find('p > img, div');
+
+  const content = [];
+
+  doms.map((i, e) => {
+    const text = $(e).text();
+    if (text) {
+      content.push(text);
+    } else if (e.name == 'img') {
+      console.log(e)
+      const src = $(e).attr('src');
+      content.push(src);
+    }
+  })
+
+  console.log(content)
+
 
 })().then(r => {
   process.exit(0)
